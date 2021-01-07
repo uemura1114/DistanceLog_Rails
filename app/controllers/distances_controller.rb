fc9cdc9eda696ad8af290b4cc7669790aacdc066
Uemura Wataru <bbwtsn.par72@gmail.com>
2021-01-07 18:23:59 +0900

    地点を登録するフォームを作成し、DBへの登録をできるようにした

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,6 +1,16 @@
 class DistancesController < ApplicationController
   def index
-    
+    @distance = Distance.new
+  end
+
+  def create
+    Distance.create(distance_params)
+  end
+
+  private
+
+  def distance_params
+    params.require(:distance).permit(:st_lat, :st_lng, :ed_lat, :ed_lng)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'distances#index'
+  get 'distances', to: 'distances#index'
+  post 'distances', to: 'distances#create'
+  
 end
