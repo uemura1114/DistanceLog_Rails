1c7cc277d08d87574e914a3af5cf863be70a35d0
Uemura Wataru <bbwtsn.par72@gmail.com>
2021-01-09 08:51:00 +0900

    anotateをインストールして実行

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: distances
+#
+#  id         :bigint           not null, primary key
+#  distance   :float(24)
+#  ed_lat     :decimal(8, 6)
+#  ed_lng     :decimal(9, 6)
+#  st_lat     :decimal(8, 6)
+#  st_lng     :decimal(9, 6)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Distance < ApplicationRecord
 
   validates :st_lat,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  name            :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#
 class User < ApplicationRecord
   #これでpassword属性とpassword_confirmation属性が追加される。
   has_secure_password
