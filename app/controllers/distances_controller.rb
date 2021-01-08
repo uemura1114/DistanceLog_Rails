688c45736b006951f89a0c5609386b3c1de13920
Uemura Wataru <bbwtsn.par72@gmail.com>
2021-01-08 13:02:57 +0900

    データ登録・削除機能を追加
    登録した際の成功メッセージを表示する機能と、削除機能を追加。

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,11 +1,18 @@
 class DistancesController < ApplicationController
   def index
     @distance = Distance.new
+    @distances = Distance.all
   end
 
   def create
     Distance.create(distance_params_add_distance)
-    binding.pry
+    redirect_to distances_path, flash: { success: 'データが追加されました'}
+  end
+
+  def destroy
+    distance = Distance.find(params[:id])
+    distance.destroy
+    redirect_to distances_path, flash: { success: 'データが削除されました'}
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'distances', to: 'distances#index'
-  post 'distances', to: 'distances#create'
+  # get 'distances', to: 'distances#index'
+  # post 'distances', to: 'distances#create'
+  resources :distances, only: [:index, :create, :destroy]
   
 end
