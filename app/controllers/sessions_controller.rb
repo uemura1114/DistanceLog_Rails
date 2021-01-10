b7ade85127a0b2240137f96618eefec77d786d6e
Uemura Wataru <bbwtsn.par72@gmail.com>
2021-01-11 08:16:19 +0900

    ログイン失敗時の処理を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to distances_path
     else
+      flash.now[:error_messages] = ['ログインに失敗しました']
       render 'home/index'
     end
   end
