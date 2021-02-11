7f0afa79cedebf133a0c359e8760f6ae83d94324
Uemura Wataru <bbwtsn.par72@gmail.com>
2021-02-11 11:16:22 +0900

    mypageの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
   end
 
   def me
+    if @current_user
+      @distances = @distances = Distance.where(user_id: @current_user.id)
+    else
+      redirect_to root_path
+    end
   end
 
   def user_params
